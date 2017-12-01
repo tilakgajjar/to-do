6f70466de972be3657cedd8c9b86b24f0ac4e103
Tilak Gajjar <tilak@Tilaks-MacBook-Pro.local>
2017-12-01 18:24:46 -0500

    entirely new app from scratch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './reducers'
+import App from './components/App'
 
 let store = createStore(todoApp)
 
-console.log(store.getState())
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
